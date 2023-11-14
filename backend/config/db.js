dba378bc123b0a4d86f15fbec2c45a2210f907e8
rapheallzz <70053913+rapheallzz@users.noreply.github.com>
2023-11-14 18:22:37 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose')
-
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
 
-   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
-    console.log(`MongoDB Connected: ${conn.connection.host}`.yellow.underline);
-   } catch (error) {
-    console.log(error);
-    process.exit(1)
-   }
-
-}
-
-module.exports = connectDB
+module.exports = connectDB;
