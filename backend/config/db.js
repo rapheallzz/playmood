3cdd75f1afb9d5245d25c1a32c5d5a3dcb76b510
rapheallzz <70053913+rapheallzz@users.noreply.github.com>
2023-11-14 18:30:43 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,8 +5,8 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: false, // Set to false to remove the deprecated warning
-      useFindAndModify: false, // Set to false to remove the deprecated warning
+      useCreateIndex: false, // Deprecated, but set to false to remove the deprecated warning
+      useFindAndModify: false, // Deprecated, but set to false to remove the deprecated warning
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
